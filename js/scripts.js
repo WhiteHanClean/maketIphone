483d56bb80b333a6419bb26efc12e8bfbb53f89f
Han <whitehanclean@gmail.com>
2021-03-15 16:11:36 +0600

    доработка аккордиона

--- js/scripts.js
+++ js/scripts.js
@@ -2,52 +2,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const tabs = () => {
 
         const cardDetailChangeElem = document.querySelectorAll('.card-detail__change');
-        const cardDetailsTitleElem = document.querySelectorAll('.card-details__title');
-        const cardImageItemElem = document.querySelector('card__image_item');
-        const cardDetailsPriceElem = document.querySelector('card-details__price');
-        const descriptionMemory = document.querySelector('description__memory');
+        const cardDetailsTitleElem = document.querySelector('.card-details__title');
+        const cardImageItem = document.querySelector('.card__image_item');
+        const cardDetailsPrice = document.querySelector('.card-details__price');
+        const descriptionMemory = document.querySelector('.description__memory')
 
         const data = [{
                 name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
                 img: 'img/iPhone-graphite.png',
-                price: 95900,
-                memoryRom: 128
+                price: 95990,
+                memoryROM: 128
             },
             {
-                name: 'Смартфон Apple iPhone 12 Pro 128GB Silver',
+                name: 'Смартфон Apple iPhone 12 Pro 256GB Silver',
                 img: 'img/iPhone-silver.png',
-                price: 120900,
-                memoryRom: 256
+                price: 120990,
+                memoryROM: 256
             },
             {
                 name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
                 img: 'img/iPhone-blue.png',
-                price: 99900,
-                memoryRom: 128
+                price: 99990,
+                memoryROM: 128
             }
         ];
 
         const deactive = () => {
             cardDetailChangeElem.forEach(btn => btn.classList.remove('active'));
         }
-
-        cardDetailChangeElem.forEach((btn, i) => {
+        cardDetailChangeElem.forEach((btn, index) => {
             btn.addEventListener('click', () => {
-
                 if (!btn.classList.contains('active')) {
                     deactive();
                     btn.classList.add('active');
-                    cardDetailsTitleElem.textContent = data[i].name;
-                  
-                    cardImageItemElem.src = data[i].img;
-                    cardImageItemElem.alt = data[i].name;
-                    cardDetailsPriceElem.textContent = data[i].price + '₽';
-                    descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memoryRom} ГБ`;
+
+                    cardDetailsTitleElem.textContent = data[index].name;
+                    cardImageItem.src = data[index].img;
+                    cardImageItem.alt = data[index].name;
+                    cardDetailsPrice.textContent = data[index].price + 'R';
+                    descriptionMemory.textContent = `Встроенная память (ROM)${data[index].memoryROM} ГБ`
                 }
             });
         })
-
-
     };
+
+    const accordion = () => {
+        const characteristicsTitle = document.querySelectorAll('.characteristics__title');
+        const characteristicsDescription = document.querySelectorAll('.characteristics__description');
+
+        characteristicsTitle.forEach((elem, i) => {
+            elem.addEventListener('click', () => {
+                elem.classList.toggle('active');
+                characteristicsDescription[i].classList.toggle('active');
+            });
+        });
+    };
+
+
     tabs();
-});
+    accordion();
+})
